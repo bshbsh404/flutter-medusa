d36bf7cc4f08ecab7eecd390c3b0262297ff5ead
gunkev <kevinenzapdi@gmail.com>
2023-06-14 05:24:52 +0100

    added app utils

---
+++ lib/utils/utilities.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// Colors
+Color kWhite = Color(0xffffffff);
+Color kPrimary = Color(0xffFB1164);
+Color kPrimaryLight = Color(0xffd36e99);
+Color kDark = Color(0xFF605A65);
+Color kBlack = Color(0xFF000000);
+
